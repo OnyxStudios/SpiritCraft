67a5eb0665ee1805df4c696b122dd1af323239a8
Traverse-Joe <roxxanrebirth@gmail.com>
2020-09-19 20:44:11 -0700

    added descriptor param for aspects

--- src/main/java/dev/onyxstudios/spiritcraft/api/aspects/Aspect.java
+++ src/main/java/dev/onyxstudios/spiritcraft/api/aspects/Aspect.java
@@ -6,17 +6,20 @@ public class Aspect {
 
     protected Identifier id;
     protected int color;
+    protected String descriptor;
     protected Aspect[] parents;
 
     /**
      * Create a new aspect type
      * @param id - An identifier used to register the aspect and it's resources (Icon)
      * @param color - The fluid & item color of the aspect
+     * @param descriptor - A descriptor/Key about the aspect
      * @param parents - Aspects used to make this aspect
      */
-    public Aspect(Identifier id, int color, Aspect... parents) {
+    public Aspect(Identifier id, int color, String descriptor, Aspect... parents) {
         this.id = id;
         this.color = color;
+        this.descriptor = descriptor;
         this.parents = parents;
         SpiritCraftAspects.ASPECTS.put(id, this);
     }
@@ -27,6 +30,10 @@ public class Aspect {
 
     public int getColor() {
         return color;
+    }
+
+    public String getDescriptor() {
+        return descriptor;
     }
 
     public Aspect[] getParents() {
